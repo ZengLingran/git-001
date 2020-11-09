fcc29f06c8d99c4eabed876c93452dd8131b1eee
ZengLR <898802801@qq.com>
2020-11-09 09:02:26 +0800

    本地工程添加到git

---
+++ testGit/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+/**
+ * Created by ZengLR on 2020/11/9.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.print("hello 001");
+        System.out.print("hello 002");
+        System.out.print("hello 003");
+        System.out.print("hello 004");
+    }
+}
